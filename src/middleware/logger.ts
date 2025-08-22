fc80b88ffa3c38ad5a7b8aa7f690f550a3ea6ed3
Wyatt <wyatt.furois@gmail.com>
2025-08-22 11:13:46 -0500

    vercel-ified

--- src/middleware/logger.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const logger = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
